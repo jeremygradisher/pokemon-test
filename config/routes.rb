bfea3c42ba15eb27ff96951c8573097f55dd6d24
jeremygradisher <jeremygradisher@gmail.com>
2020-01-06 22:33:20 +0000

    Ready for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq" 
 
   resources :pokemons, only: [:index]
+  root 'pokemons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'pokemons/upload', to: 'pokemons#upload'
   post 'pokemons/destroy_all', to: 'pokemons#destroy_all'
